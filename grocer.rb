8a62760f5f70a7313e1419d7760715f3f32401da
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 18:32:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ end
 
 def apply_coupons(cart, coupons)
   # code here
-  puts cart
+  #puts cart
   coupons_applied = {}
   coupons_applied = coupons_applied.merge(cart)
   coupons.each do |coupon|
