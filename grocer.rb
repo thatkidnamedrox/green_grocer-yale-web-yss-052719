2d444057822f05f3ed7dccc83ff83d705cb63629
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 18:41:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,6 +33,7 @@ def apply_coupons(cart, coupons)
     coupons_applied[item_coupon][:count] ? coupons_applied[item_coupon][:count] += 1 : coupons_applied[item_coupon][:count] = 1
 
     #count = cart[item][:count]
+    coupons_applied[item] = {}
     coupons_applied[item][:price] = coupon[:cost]
     coupons_applied[item][:clearance] = cart[item][:clearance]
     #coupons_applied[item_coupon][:count] ? coupons_applied[item_coupon][:count] += 1 : coupons_applied[item_coupon][:count] = 1
