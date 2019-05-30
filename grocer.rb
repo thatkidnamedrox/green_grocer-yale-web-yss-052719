0c6bfa45b45c0a48fab8c488c69c0222e8edb9bc
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 19:29:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,8 +40,8 @@ def apply_coupons(cart, coupons)
     coupons_applied[item] ||= {}
     coupons_applied[item][:price] = cart[item][:price]
     coupons_applied[item][:clearance] = cart[item][:clearance]
-
-    if cart[item][:count] >= coupon[:num]
+    coupons_applied[item][:count] ? nil : coupons_applied[item][:count] = cart[item][:count]
+    if coupons_applied[item][:count] >= coupon[:num]
       puts "applied", cart[item][:count], coupon[:num]
       coupons_applied[item_coupon] ||= {}
       coupons_applied[item_coupon][:price] = coupon[:cost]
@@ -49,7 +49,7 @@ def apply_coupons(cart, coupons)
       coupons_applied[item_coupon][:count] ? nil : coupons_applied[item_coupon][:count] = 0
       coupons_applied[item_coupon][:count] += 1
 
-      coupons_applied[item][:count] ? nil : coupons_applied[item][:count] = cart[item][:count]
+      #coupons_applied[item][:count] ? nil : coupons_applied[item][:count] = cart[item][:count]
       coupons_applied[item][:count] -= coupon[:num]
     else
       coupons_applied[item][:count] ? nil : coupons_applied[item][:count] = cart[item][:count]
