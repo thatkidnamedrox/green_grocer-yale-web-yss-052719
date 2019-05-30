99c81b6ae98c22102afa2c346b6b6c37dcae9605
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 18:37:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,14 +24,14 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
     cart[item] ? nil : break
-    
+
     item_coupon = "#{item} W/COUPON"
-  
+
     coupons_applied[item_coupon] ? nil : coupons_applied[item_coupon] = {}
     # coupons_applied[item_coupon][:price] = coupon[:cost]
     # coupons_applied[item_coupon][:clearance] = cart[item][:clearance]
     # coupons_applied[item_coupon][:count] ? coupons_applied[item_coupon][:count] += 1 : coupons_applied[item_coupon][:count] = 1
-    # 
+    #
     # coupons_applied[item][:count] = cart[item][:count] - coupon[:num]
     # coupons_applied[item][:count]
   end
