f06a29c48fa14c0cf2f70b679f9da1627b9f6e9c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 18:38:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,7 +33,7 @@ def apply_coupons(cart, coupons)
     coupons_applied[item_coupon][:count] ? coupons_applied[item_coupon][:count] += 1 : coupons_applied[item_coupon][:count] = 1
 
     count = cart[item][:count]
-    coupons_applied[item][:count] = count - coupon[:num]
+    #coupons_applied[item][:count] = count - coupon[:num]
     # coupons_applied[item][:count]
   end
   #puts coupons_applied, "", cart
