51e7f4d2109e0521c1a2caf912ed8da28b0b0771
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 19:28:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -83,7 +83,7 @@ def checkout(cart, coupons)
   consolidated = consolidate_cart(cart)
   coupons_applied = apply_coupons(consolidated, coupons)
   clearance_applied = apply_clearance(coupons_applied)
-  puts cart, "", consolidated, "", coupons, coupons_applied, "", clearance_applied
+  #puts cart, "", consolidated, "", coupons, coupons_applied, "", clearance_applied
   total = 0.0
   clearance_applied.each {|item, info| total += info[:price] * info[:count].clamp(0, 10000000)}
   total
